a83bad43feb7a2b5f7fb4d6b65954aa79014c019
Fatih Altinok <fatihaltinok@live.com>
2023-05-26 16:37:09 +0300

    feat: Add second featured row in Services page

--- components/ServicesPage/ServicesPage.tsx
+++ components/ServicesPage/ServicesPage.tsx
@@ -1,11 +1,13 @@
 import BackgroundGrid from '@components/common/BackgroundGrid'
 import Card from '@components/common/Card'
+import API from '@components/icons/API/API'
 import CMS from '@components/icons/CMS/CMS'
 import Desktop from '@components/icons/Desktop'
 import Email from '@components/icons/Email/Email'
 import LCD from '@components/icons/LCD/LCD'
 import Management from '@components/icons/Management/Management'
 import OnlineShopping from '@components/icons/OnlineShopping/OnlineShopping'
+import Optimization from '@components/icons/Optimization/Optimization'
 import Screen from '@components/icons/Screen'
 import Search from '@components/icons/Search/Search'
 import Smartphone from '@components/icons/Smartphone/Smartphone'
@@ -32,30 +34,33 @@ const ServicesPage: FC<ServicesPageProps> = () => {
       <div className={s.animatedBg}>
         <BackgroundGrid bgColor="black" direction="down" initialHeight={240} />
       </div>
-      <Container className={s.container}>
-        <div className={s.featuredRow}>
-          <Card
-            className={s.featuredCol}
-            icon={<Desktop />}
-            title="ECOMMERCE"
-            description="We build future-forward e-commerce solutions – shopper-friendly web and mobile apps that will help your business succeed in an ever-changing e-commerce world. All powered by MACH. "
-          />
-          <Card
-            className={s.featuredCol}
-            icon={<World />}
-            title="FUTURE WEB"
-            description="Partner with us for future web technologies, Metaverse, NFT and anything your heart desires. We’ll bring intelligent frameworks that are bold, smart and ready for the future."
-          />
-          <Card
-            className={s.featuredCol}
-            icon={<Screen />}
-            title="WEB APPS"
-            description="Custom web app development that integrates existing applications and optimizes workflow. From back-end development to front-end design. Nothing off-the-shelf – fully customized."
-          />
-        </div>
-      </Container>
 
       <main>
+        <section>
+          <Container className={s.container}>
+            <div className={s.featuredRow}>
+              <Card
+                className={s.featuredCol}
+                icon={<Desktop />}
+                title="ECOMMERCE"
+                description="We build future-forward e-commerce solutions – shopper-friendly web and mobile apps that will help your business succeed in an ever-changing e-commerce world. All powered by MACH. "
+              />
+              <Card
+                className={s.featuredCol}
+                icon={<World />}
+                title="FUTURE WEB"
+                description="Partner with us for future web technologies, Metaverse, NFT and anything your heart desires. We’ll bring intelligent frameworks that are bold, smart and ready for the future."
+              />
+              <Card
+                className={s.featuredCol}
+                icon={<Screen />}
+                title="WEB APPS"
+                description="Custom web app development that integrates existing applications and optimizes workflow. From back-end development to front-end design. Nothing off-the-shelf – fully customized."
+              />
+            </div>
+          </Container>
+        </section>
+
         <section>
           <FeaturedContent
             title="MACH Principles"
@@ -84,6 +89,31 @@ const ServicesPage: FC<ServicesPageProps> = () => {
             ]}
             extraContent={<MachDiagram />}
           />
+        </section>
+
+        <section>
+          <Container className={s.container}>
+            <div className={s.featuredRow}>
+              <Card
+                className={s.featuredCol}
+                icon={<API />}
+                title="API Intergrations"
+                description="One step ahead of the latest developments to create seamless integration of content, APIs, and commerce via headless commerce. The result? Enjoyable shopping experiences across all digital touchpoints."
+              />
+              <Card
+                className={s.featuredCol}
+                icon={<CMS />}
+                title="CMS"
+                description="Custom-built CMS solutions from application development and plugins to integrations and migrations – across all types of industries. Just ask."
+              />
+              <Card
+                className={s.featuredCol}
+                icon={<Optimization />}
+                title="Site Maintenance"
+                description="Our experts provide ongoing maintenance, monitoring, and support to ensure that your custom websites and apps are optimized – including testing and debugging, security protocol updates, database maintenance, SEO and more."
+              />
+            </div>
+          </Container>
         </section>
       </main>
     </div>
