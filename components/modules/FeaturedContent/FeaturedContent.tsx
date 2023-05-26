fbf5a83d4183dc456080f144709de123c8defd8e
Fatih Altinok <fatihaltinok@live.com>
2023-05-26 15:57:36 +0300

    feat: Add bg direction and extra content to FeaturedContent component

--- components/modules/FeaturedContent/FeaturedContent.tsx
+++ components/modules/FeaturedContent/FeaturedContent.tsx
@@ -2,13 +2,14 @@
 
 import AnimatedBackgroundGrid from '@components/common/AnimatedBackgroundGrid'
 import BackgroundGrid from '@components/common/BackgroundGrid'
+import { BackgroundGridProps } from '@components/common/BackgroundGrid/BackgroundGrid'
 import LearnMore from '@components/ui/LearnMore'
 import cn from 'classnames'
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, ReactNode, useEffect, useRef, useState } from 'react'
 
 import s from './FeaturedContent.module.css'
 
-interface FeaturedContentProps {
+interface FeaturedContentProps extends Pick<BackgroundGridProps, 'direction'> {
   title?: string
   link?: string
   description?: string
@@ -16,12 +17,16 @@ interface FeaturedContentProps {
     title: string
     description: string
   }[]
+  extraContent?: ReactNode
 }
+
 const FeaturedContent: FC<FeaturedContentProps> = ({
   title,
   link,
   description,
   content,
+  extraContent,
+  direction,
 }) => {
   const [elementHeight, setElementHeight] = useState(458)
   const [elementWidth, setElementWidth] = useState(1367)
@@ -47,7 +52,7 @@ const FeaturedContent: FC<FeaturedContentProps> = ({
   return (
     <>
       <div className={s.root}>
-        <div className={cn(s.content, 'lg:grid-cols-4')}>
+        <div className={cn(s.content, 'lg:grid-cols-4 gap-12')}>
           <div className={cn(s.highlight, 'lg:col-span-1')}>
             {title && (
               <h2 className={cn(s.heading, 'headingTreatmentSecondary')}>
@@ -66,19 +71,26 @@ const FeaturedContent: FC<FeaturedContentProps> = ({
             )}
           </div>
           <ol
-            className={cn(s.ol, 'lg:col-span-3', 'lg:grid', 'lg:grid-cols-3')}
+            className={cn(
+              s.ol,
+              'lg:grid gap-12',
+              extraContent
+                ? 'lg:col-start-1 lg:col-end-3 lg:pr-24'
+                : 'lg:col-span-3 lg:grid-cols-3'
+            )}
           >
             {content?.map((item, i) => (
               <li className={s.item} key={item.title}>
-                <h3 className={s.h3}>
+                <h3 className={cn(s.h3, extraContent && 'w-3/4')}>
                   <span className={s.subHeading}>{item.title}</span>
                 </h3>
                 <p className={s.description}>{item.description}</p>
               </li>
             ))}
           </ol>
+          <div className="lg:col-start-3 lg:col-end-5">{extraContent}</div>
         </div>
-        <BackgroundGrid bgColor="rgb(43, 34, 114)" />
+        <BackgroundGrid bgColor="rgb(43, 34, 114)" direction={direction} />
       </div>
     </>
   )
