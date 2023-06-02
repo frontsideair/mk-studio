2826b9fee3705fcc857a75529355eee75df1ee6b
Fatih Altinok <fatihaltinok@live.com>
2023-06-02 14:11:54 +0300

    refactor: Use higher level component

--- components/modules/FeaturedRow/FeaturedRow.tsx
+++ components/modules/FeaturedRow/FeaturedRow.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import AnimatedBackgroundGrid from '@components/common/AnimatedBackgroundGrid'
+import BackgroundGrid from '@components/common/BackgroundGrid'
 import cn from 'classnames'
 import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
 
 import s from './FeaturedRow.module.css'
 
@@ -11,35 +10,11 @@ interface FeaturedRowProps {
   title?: string
   images?: any[]
 }
+
 export default function FeauturedRow({ title, images = [] }: FeaturedRowProps) {
-  const [elementHeight, setElementHeight] = useState(458)
-  const [elementWidth, setElementWidth] = useState(1367)
-  const ref = useRef<HTMLDivElement>(null)
-  console.log({ images })
-
-  useEffect(() => {
-    const handleResize = () => {
-      setElementWidth(innerWidth || ref.current?.clientWidth || elementWidth)
-      setElementHeight(ref.current?.clientHeight || elementHeight)
-    }
-    window.addEventListener('resize', handleResize)
-    setElementHeight(ref.current?.clientHeight ?? 394)
-    setElementWidth(ref.current?.clientWidth ?? innerWidth)
-  }, [
-    elementHeight,
-    elementWidth,
-    ref.current?.clientHeight,
-    ref.current?.clientWidth,
-  ])
-
   return (
     <div className={s.root}>
-      <AnimatedBackgroundGrid
-        bgColor="transparent"
-        bgWidth={elementWidth}
-        bgHeight={elementHeight}
-        direction="down"
-      />
+      <BackgroundGrid bgColor="transparent" direction="down" />
       <div className={cn(s.content)}>
         <div className={cn(s.highlight)}>
           {title && (
